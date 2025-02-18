bcc3557b51927545ee5689fd0b9205e58da7174b
ywallis <yann.wallis@gmail.com>
2025-02-18 08:38:03 +0800

    Added char counting

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+
+def get_text(path: str) -> str:
+    with open(path) as f:
         file_contents = f.read()
+        return file_contents
 
-        counter = 0
-        for file in file_contents.split():
-            counter += 1
+def count_all(text: str) -> int:
 
-        print(counter)
+    return len(text.split())
+
+
+def count_individual(text: str) -> dict[str, int]:
+
+    output: dict[str, int] = {}
+    for char in text:
+        lc_char = char.lower()
+        if lc_char in output:
+            output[lc_char] += 1
+        else:
+            output[lc_char] = 1
+
+    return output
+
+def main():
+
+    text = get_text("books/frankenstein.txt")
+    print(count_all(text))
+
+    print(count_individual(text))
+
 main()
